14970c0b0dc45feae1f713857ce905c2d40bd91e
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-03-14 03:03:14 +0900

    add bootstrap

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600,
+    webPreferences: {
+      nodeIntegration: false
+    }})
   mainWindow.webContents.openDevTools()
 
   // and load the index.html of the app.
